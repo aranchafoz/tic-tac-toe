cf5dd21886c70f7dfb8e01e945e4c02e80426eb7
Arancha <aranchunfli11foz@gmail.com>
2021-09-06 16:36:07 +0100

    fix: update server port

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Well done!');
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('The application is listening on port 3000!');
 })
