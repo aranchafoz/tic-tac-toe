0ddbcc841c702d63db5c34836249901de8bc29d9
Arancha <aranchunfli11foz@gmail.com>
2021-09-07 15:02:43 +0100

    feat: validate if input board meet basic requirements

--- index.ts
+++ index.ts
@@ -1,12 +1,29 @@
 import express from 'express';
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
+function validateBoardContent(board: string): boolean {
+    return /^[xo ]{9}$/.test(board);
+}
+
 app.get('/', (req, res) => {
-    res.send('Well done!');
+    
+    // Access the provided 'page' and 'limt' query parameters
+    const board = req.query.board?.toString();
+    console.log('board: ', board)
+    console.log(board?.length)
+
+    if (!board || !validateBoardContent(board)) {
+        res.status(400).send('Invalid board');
+    } else {
+        res.status(200).send('Valid board!');
+    }
 })
 
 app.listen(port, () => {
-    console.log('The application is listening on port 3000!');
+    console.log(`The application is listening on port ${port}!`);
 })
