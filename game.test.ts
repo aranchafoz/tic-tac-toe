67572500d86e137c73b81dba647c1d7a5613d3ba
Arancha <aranchunfli11foz@gmail.com>
2021-09-07 17:37:49 +0100

    feat: add tests to cpu fork move

--- game.test.ts
+++ game.test.ts
@@ -1,23 +1,33 @@
 import Game from './game';
 
 test.each([
-    {input: 'o o  xx  ', expected: 'ooo  xx  '},
-    {input: ' o xox   ', expected: ' o xox o '},
-    {input: '    o oxx', expected: '  o o oxx'},
-  ])('Win - $input', ({input, expected}) => {
-    const game = new Game(input);
-    game.storeLines();
-    const nextMove = game.calculateNextMove();
-    expect(nextMove).toBe(expected);
-  });
+  { input: 'o o  xx  ', expected: 'ooo  xx  ' },
+  { input: ' o xox   ', expected: ' o xox o ' },
+  { input: '    o oxx', expected: '  o o oxx' },
+])('Win - $input', ({ input, expected }) => {
+  const game = new Game(input);
+  game.storeLines();
+  const nextMove = game.calculateNextMove();
+  expect(nextMove).toBe(expected);
+});
 
 test.each([
-    {input: 'o  o  x x', expected: 'o  o  xox'},
-    {input: '  x oxo  ', expected: '  x oxo o'},
-    {input: 'x     oox', expected: 'x   o oox'},
-  ])('Block - $input', ({input, expected}) => {
-    const game = new Game(input);
-    game.storeLines();
-    const nextMove = game.calculateNextMove();
-    expect(nextMove).toBe(expected);
-  });
+  { input: 'o  o  x x', expected: 'o  o  xox' },
+  { input: '  x oxo  ', expected: '  x oxo o' },
+  { input: 'x     oox', expected: 'x   o oox' },
+])('Block - $input', ({ input, expected }) => {
+  const game = new Game(input);
+  game.storeLines();
+  const nextMove = game.calculateNextMove();
+  expect(nextMove).toBe(expected);
+});
+
+test.each([
+  { input: 'xx  o   o', expected: 'xxo o   o' },
+  { input: 'x   ox  o', expected: 'xo  ox  o' },
+])('Fork - $input', ({ input, expected }) => {
+  const game = new Game(input);
+  game.storeLines();
+  const nextMove = game.calculateNextMove();
+  expect(nextMove).toBe(expected);
+});
