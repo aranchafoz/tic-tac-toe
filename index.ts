97208d7a49b7e169b08558482f7c81c323227676
Arancha <aranchunfli11foz@gmail.com>
2021-09-07 15:12:13 +0100

    feat: validate if its plausibly Os turn

--- index.ts
+++ index.ts
@@ -10,14 +10,19 @@ function validateBoardContent(board: string): boolean {
     return /^[xo ]{9}$/.test(board);
 }
 
+function isPlausiblyOsTurn(board: string): boolean {
+    return board.includes(' ');
+}
+
 app.get('/', (req, res) => {
     
     // Access the provided 'page' and 'limt' query parameters
     const board = req.query.board?.toString();
     console.log('board: ', board)
     console.log(board?.length)
-
-    if (!board || !validateBoardContent(board)) {
+    if (!board) {
+        res.status(400).send('It is mandatory to send by parameter the board')
+    } else if (!validateBoardContent(board) || !isPlausiblyOsTurn(board)) {
         res.status(400).send('Invalid board');
     } else {
         res.status(200).send('Valid board!');
